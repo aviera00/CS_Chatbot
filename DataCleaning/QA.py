05b6f83a97ff3a09d34d910d7f89eded1e4bf5ba
Alonso <dac323@lehigh.edu>
2022-01-19 18:53:01 -0500

    python_script

--- DataCleaning/QA.py
+++ DataCleaning/QA.py
@@ -1,6 +1,6 @@
 import numpy as np
 import pandas as pd
 
-Questions_109 = open('piazza_questions_cse109.txt', encoding='utf8')
-Questions_109 = pd.read_csv('piazza_questions_cse109.txt', sep='|')
-print(Questions_109.iloc[0])
+Questions_109 = pd.read_csv('piazza_questions_cse109.txt', sep='|',header=None)
+Questions_109.columns=["ID","Problem","Subject","Error","Answer","pid"]
+Questions_109.to_csv('QA109.csv')
