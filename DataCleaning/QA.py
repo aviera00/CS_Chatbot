6dbee9a5b18d9e5e2ed3a98e770dcfa323527e77
Alonso <dac323@lehigh.edu>
2022-01-20 14:31:27 -0500

    Python Cleaning Final

--- DataCleaning/QA.py
+++ DataCleaning/QA.py
@@ -3,4 +3,4 @@ import pandas as pd
 
 Questions_109 = pd.read_csv('piazza_questions_cse109.txt', sep='|',header=None)
 Questions_109.columns=["ID","Problem","Subject","Error","Answer","pid"]
-Questions_109.to_csv('QA109.csv')
+Questions_109.to_csv('QA109.csv',index=False,sep='|')
