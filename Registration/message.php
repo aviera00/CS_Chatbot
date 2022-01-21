4b6409dbff9980e04a828e5bd53f84e1a72e67a7
Alonso <dac323@lehigh.edu>
2022-01-20 22:29:44 -0500

    chat working only with keywords

--- Registration/message.php
+++ Registration/message.php
@@ -1,7 +1,63 @@
-
-
 <?php
 
+// connecting to database
+$conn = mysqli_connect("localhost", "root", "root", "hw2_witAI") or die("Database Error");
+
+// getting user message through ajax
+$getMesg = mysqli_real_escape_string($conn, $_POST['text']);
+
+debug_to_console($getMesg);
+//Index Code
+//$input_utterance= 'I like finance';
+
+$witRoot = "https://api.wit.ai/message?";
+$witVersion = '20200804';
+
+$witURL =  $witRoot. "v=". $witVersion . "&q=". urlencode($getMesg);
+
+$ch = curl_init();
+$header = array();
+$header[] = 'Authorization: Bearer MF6I76FL2MAG2QFBOJ7O4K3UKGZBZW2D';
+
+curl_setopt($ch, CURLOPT_URL, $witURL);
+curl_setopt($ch, CURLOPT_HTTPHEADER,$header); 
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, true); 
+$certificate = "C:\MAMP\htdocs\demo\CS_Chatbot\cacert.pem";
+curl_setopt($ch, CURLOPT_CAINFO, $certificate);
+curl_setopt($ch, CURLOPT_CAPATH, $certificate);
+
+//if a curl error is thrown
+if(curl_errno($ch)){
+    echo 'Curl error: ' . curl_error($ch);
+}
+
+$server_output = curl_exec($ch); 
+
+
+curl_close ($ch);
+$server_decoded_rsp = json_decode($server_output)->entities->{"issues:issues"};
+$response = "";
+$answer='';
+for ($i = 0; $i < count($server_decoded_rsp); $i++){
+    $keyword = $server_decoded_rsp[$i]->value;
+	$con_db = mysqli_connect("localhost", "root", "root", "hw2_witAI"); 
+  	/*if (mysqli_connect_errno($con_db)) {
+    	echo "Failed to connect  to MYSql:" . mysqli_connect_error();
+  	}*/
+    
+  	$sql_command = "SELECT answer FROM response WHERE keyword = '{$keyword}'";
+  	$result = mysqli_query($con_db, $sql_command);
+  	$num_rows = mysqli_num_rows($result);
+  	if ($num_rows > 0) {
+    	$row = mysqli_fetch_array($result);
+    	$answer = $row[0];
+  	} else {
+    	echo "failed";
+  	}
+  	mysqli_close($con_db);
+}
+
+//end of index code
 function debug_to_console($data) {
     $output = $data;
     if (is_array($output))
@@ -11,13 +67,7 @@ function debug_to_console($data) {
 }
 
 
-// connecting to database
-$conn = mysqli_connect("localhost", "root", "root", "hw2_witAI") or die("Database Error");
 
-// getting user message through ajax
-$getMesg = mysqli_real_escape_string($conn, $_POST['text']);
-
-debug_to_console($getMesg);
 //checking user query to database query
 $check_data = "SELECT answer FROM response WHERE keyword = '{$getMesg}'";
 
@@ -29,7 +79,7 @@ if(mysqli_num_rows($run_query) > 0){
     $fetch_data = mysqli_fetch_assoc($run_query);
     //storing replay to a varible which we'll send to ajax
     $replay = $fetch_data['answer'];
-    echo $replay;
+    echo $answer;
 }else{
     echo "Sorry can't be able to understand you!";
 }
